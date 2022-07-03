da9e7dcb64db57d1c1ab3e876105e0e249c7bcbc
MildLamb <1902524390@qq.com>
2022-07-03 15:54:40 +0800

    2022/7/03

--- springboot-04-startup/src/main/java/com/mildlamb/Springboot04StartupApplication.java
+++ springboot-04-startup/src/main/java/com/mildlamb/Springboot04StartupApplication.java
@@ -2,12 +2,18 @@ package com.mildlamb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class Springboot04StartupApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot04StartupApplication.class, args);
+        args = new String[]{"test.value=kindred"};
+        ConfigurableApplicationContext run = SpringApplication.run(Springboot04StartupApplication.class, args);
+//        Properties properties = System.getProperties();
+//        properties.list(System.out);
     }
 
 }
